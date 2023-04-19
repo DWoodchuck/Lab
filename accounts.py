5f83cd48d1fd5d86608a6c4b632ff4112813be05
DWoodchuck <dylanwoodring123@gmail.com>
2023-04-19 12:48:16 -0500

    Created first file added code to functions

--- accounts.py
+++ accounts.py
@@ -1,23 +1,32 @@
-#class Account:
+class Account:
 
-    #def __init__(self,name):
-        #self.__account_name = name
-        #self.__account_balance = 0
-
-
-    #def deposit(self,amount):
-        #if self.__amount <= 0:
+    def __init__(self,name):
+        self.__account_name = name
+        self.__account_balance = 0
 
 
-
-
-    #def withdraw(self,amount):
-
-
-    #def get_balance(self):
+    def deposit(self,amount):
+        if self.__amount <= 0:
+            False
+        else:
+            self.__account_balance += self.__amount
+            True
 
 
 
-    #def get_name(self):
 
-print('GIT')
+    def withdraw(self,amount):
+        if self.__amount <= 0 or self.__amount > self.__account_balance:
+            False
+        else:
+            self.account_balance -= self.__amount
+            True
+
+    def get_balance(self):
+        return self.__account_balance
+
+
+
+    def get_name(self):
+        return name
+
