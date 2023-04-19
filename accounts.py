acf116251fd47185106c2969cf30b8e1bc2b8483
DWoodchuck <dylanwoodring123@gmail.com>
2023-04-19 11:10:10 -0500

    Created first file

---
+++ accounts.py
@@ -0,0 +1,23 @@
+#class Account:
+
+    #def __init__(self,name):
+        #self.__account_name = name
+        #self.__account_balance = 0
+
+
+    #def deposit(self,amount):
+        #if self.__amount <= 0:
+
+
+
+
+    #def withdraw(self,amount):
+
+
+    #def get_balance(self):
+
+
+
+    #def get_name(self):
+
+print('GIT')
